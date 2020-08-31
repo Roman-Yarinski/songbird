5e777cfa8eee223011662e367d4c86b7c530a005
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-08-31 21:49:13 +0300

    init: add block with bird infimation

--- songbird/src/components/Bird/Bird.jsx
+++ songbird/src/components/Bird/Bird.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./Bird.module.css";
+import BirdData from "../../data";
 
 const Bird = () => {
   return (
@@ -14,19 +15,26 @@ const Bird = () => {
 };
 
 const BirdRes = (props) => {
+  const level = BirdData[props.level];
+  const bird = BirdData[props.level][props.bird];
   return (
-    <>
+    <div className={s.bird}>
       <div className={s.birdInfo}>
-        <img className={s.birdInfo_img} src={props.img}></img>
+        <img
+          className={s.birdInfo_img}
+          src={bird.image}
+          width="200"
+          height="150"
+        ></img>
         <div className={s.birdInfo_Naming}>
-          <p className={s.birdInfo_Name}></p>
-          <p className={s.birdInfo_NameLA}></p>
-          <audio src="/"></audio>
+          <p className={s.birdInfo_Name}>{bird.name}</p>
+          <p className={s.birdInfo_NameLA}>{bird.species}</p>
+          <audio src={bird.audio} controls className={s.audioBird}></audio>
         </div>
       </div>
-      <p className={s.birdInfo_text}></p>
-    </>
+      <p className={s.birdInfo_text}>{bird.description}</p>
+    </div>
   );
 };
 
-export default Bird;
+export { BirdRes, Bird };
