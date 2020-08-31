fc36719f84d41406a21ec118bbcf07c604dae500
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-08-31 21:51:42 +0300

    init: add topics

--- songbird/src/components/Topic/Topic.jsx
+++ songbird/src/components/Topic/Topic.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import s from "./Topic.module.css";
 
-const Topic = () => {
-  return <div className={s.topic}>
-    <TopicItem topic='Разминка' />
-    <TopicItem topic='Воробьиные' />
-    <TopicItem topic='Лесные птицы' />
-    <TopicItem topic='Певчие птицы' />
-    <TopicItem topic='Хищные птицы' />
-    <TopicItem topic='Морские птицы' />
-  </div>;
+const Topic = (props) => {
+  return (
+    <div className={s.topic}>
+      <TopicItem topic="Разминка" numder={0} current={props.page} />
+      <TopicItem topic="Воробьиные" numder={1} current={props.page} />
+      <TopicItem topic="Лесные птицы" numder={2} current={props.page} />
+      <TopicItem topic="Певчие птицы" numder={3} current={props.page} />
+      <TopicItem topic="Хищные птицы" numder={4} current={props.page} />
+      <TopicItem topic="Морские птицы" numder={5} current={props.page} />
+    </div>
+  );
 };
 
 const TopicItem = (props) => {
-  return (
-  <div className={s.topicItem}>
-    <p className={s.topicName}>{props.topic}</p>
-  </div>)
+  if (props.numder === props.current) {
+    return (
+      <div className={s.topicItem +' '+ 'current'}>
+        <p className={s.topicName}>{props.topic}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className={s.topicItem}>
+        <p className={s.topicName}>{props.topic}</p>
+      </div>
+    );
+  }
 };
 
 export default Topic;
