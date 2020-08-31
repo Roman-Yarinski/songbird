64491b5d00479870b169f00b37b10d0072c062b7
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-08-31 21:48:32 +0300

    init: add block wthi answers

--- songbird/src/components/Answer/Answer.jsx
+++ songbird/src/components/Answer/Answer.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import s from "./Answer.module.css";
 
-const Answer = (props) => {
-  return (
-    <div className={s.answer}>
-      <AnswerItem />
-      <AnswerItem />
-      <AnswerItem />
-      <AnswerItem />
-      <AnswerItem />
-      <AnswerItem />
-    </div>
-  );
-};
 
 const AnswerItem = (props) => {
   return (
-    <div className={s.answerItem}>
+    <button className={s.answerItem} id={props.number} onClick={props.clickHandler} disabled={props.disabled}>
       <span className={s.dot} />
-      <p className={s.answerItem_word}>{props.bird || "Ivolga"}</p>
-    </div>
+      <p className={s.answerItem_word} id={props.number}>{props.birdName || "BirdName"}</p>
+    </button>
   );
 };
 
-export default Answer;
+export default AnswerItem;
